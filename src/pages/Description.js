111a367546ea485cb43915955c2de5e3d38481c1
Muhammadu Amhar <75868135+thecodemonster1@users.noreply.github.com>
2024-04-12 19:54:19 +0530

    updated

--- src/pages/Description.js
+++ src/pages/Description.js
@@ -8,42 +8,8 @@ import { useNavigate } from "react-router-dom";
 const Description = ({username}) => {
   const navigate = useNavigate();
   return (
-    <TomatoAPI username={username} />
+    <TomatoAPI />
   );
 };
 
 export default Description;
-
- {/* 
-    <div className="description-container">
-    <h1>🍅Welcome to the Tomato Game! Here are the rules:🍅</h1>
-      <p>
-      🍅Hidden Equations: You'll be presented with mathematical equations, and some
-        numbers will be cleverly hidden within tomatos.
-      </p>
-      <p>
-      🍅Find the Numbers: Your task is to identify the hidden numbers within the
-        tomatoes.
-      </p>
-      <p>
-      🍅Hearts System: You start with 5 hearts. You'll lose a heart if you fail to
-        provide a correct answer.
-      </p>
-      <p>
-      🍅Losing Hearts: Losing all hearts ends the challenge. Accuracy and speed are
-        crucial to maintaining your hearts.
-      </p>
-      <p>
-      🍅HighScoreboard: Successfully complete challenges to earn a spot on the
-        highScoreboard. Complete with others and showcase your tomato-solving skills.
-      </p>
-      <p>
-      🍅Logout:You can logout anytime using the "Logout" option in the navigation menu.
-      </p>
-      <p>
-      🍅Play the Game:Click "Play the Game"to begin your Tomato game adventure.
-      </p>
-      <button className="play-button" onClick={() => navigate("/TomatoAPI")}>
-        Play the Game
-      </button>
-    </div> */}
